ed68d88d2bb111c074edf60a4041e7237cff081b
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-04-04 14:31:16 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace debugpractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+
+            }
+        }
+    }
+}
